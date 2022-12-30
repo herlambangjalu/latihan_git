7a711c11f5efcf6ab0e26bb6113bd9cbbbf7bf7b
herlambangjalu <121593215+herlambangjalu@users.noreply.github.com>
2022-12-30 21:38:51 +0700

    Create latihan.php

---
+++ latihan.php
@@ -0,0 +1,10 @@
+<?php
+
+echo "<h2> Identitas Diri </h2>
+echo "Nama : Herlambang Jalu Prasetyo" "<br>";
+echo "Nama Panggilan : Jalu" "<br>";
+echo "TTL : Wonogiri, 26 November 1997" "<br>";
+echo "Jenis Kelamin : Laki-laki" "<br>";
+echo "Alamat : Jatisrono, Wonogiri" "<br>";
+
+?>
